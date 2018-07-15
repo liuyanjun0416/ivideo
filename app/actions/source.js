070ef1dfb4882a58e7dc492a2a82d4efad37dc56
Liu Yanjun <liuyanjun0416@users.noreply.github.com>
2018-07-15 22:38:12 +0800

    Update source.js

--- app/actions/source.js
+++ app/actions/source.js
@@ -1,12 +1,23 @@
-import * as api from '../utils/fetch';
+// import * as api from '../utils/fetch';
+import {getVipList} from '../utils/getVipList"
 
-export function getAllVideoSource() {
+export function getAllVideoSource(){
   return (dispatch) => {
-    api.source.getAllVideoSource().then(res => {
-      return dispatch({
-        type: 'GETALLVIDEOSOURCE',
-        payload: res.data
-      });
+    const vipList = getVipList();
+    return dispatch({
+      type: 'GETALLVIDEOSOURCE',
+      payload: vipList
     });
   }
 }
+
+// export function getAllVideoSource() {
+//   return (dispatch) => {
+//     api.source.getAllVideoSource().then(res => {
+//       return dispatch({
+//         type: 'GETALLVIDEOSOURCE',
+//         payload: res.data
+//       });
+//     });
+//   }
+// }
